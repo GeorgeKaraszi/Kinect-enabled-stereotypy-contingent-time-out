f252049b0024c8ba0ad278af512bbafde442aee4
davidb57 <davidchm57@gmail.com>
2015-11-08 16:06:19 -0500

    Quiet Hands Demo
    This is the quiet hands demo with the sound muting. It mutes the sound
    of any other process other than the program itself. The way this was
    done before was by process name so need to change that in the
    documentation document. With the process name only was not possible to
    mute everything but the program itself. Right now is using the process
    ID.

---
+++ QuietHands/QuietHands/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuietHands
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+
+        private System.Windows.Forms.Timer myTimer = new System.Windows.Forms.Timer();
+        private QuietHandsWindow win;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //gets all the processes ids
+            //mutes them all
+            //unmutes the process id of the current program
+            //after the windows is shown, unmutes the rest of the process ids
+            win = new QuietHandsWindow();
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process theprocess in processlist)
+            {
+                try
+                {
+                    VolumeMixer.SetApplicationMute(theprocess.Id, true);
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+
+            int nProcessID = Process.GetCurrentProcess().Id;
+
+            VolumeMixer.SetApplicationMute(nProcessID, false);
+
+            win.ShowDialog();
+
+            foreach (Process theprocess in processlist)
+            {
+                try
+                {
+                    VolumeMixer.SetApplicationMute(theprocess.Id, false);
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+
+ 
+    }
+}
