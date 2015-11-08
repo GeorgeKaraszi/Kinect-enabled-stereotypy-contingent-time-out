9867d6fa86bb7bc44baef99f2ec323cccc66e92c
George Karaszi <georgekaraszi@gmail.com>
2015-11-08 17:39:45 -0500

    Fixed the VPV patterns for Peaks and Valley's. It not allows for PVP patters too.

--- Spikes/CaptureUtil/CaptureUtil/Algorithms/PeaksAndValleys.cs
+++ Spikes/CaptureUtil/CaptureUtil/Algorithms/PeaksAndValleys.cs
@@ -96,45 +96,6 @@ namespace CaptureUtil.Algorithms
 
         //--------------------------------------------------------------------------------
         /// <summary>
-        /// Processes and fixes the Peaks and Valley list of duplicates and 
-        /// incorrect entries.
-        /// </summary>
-        /// <param name="pvList">List containing tuples of indexes and values</param>
-        private void PostProcessPvList(List<Tuple<int, double>> pvList)
-        {
-            // Must first look for points that are not peaks nor valleys.
-            Tuple<int, double> prev = new Tuple<int, double>(-1, -1.0);
-            for (int i = 0; i < pvList.Count; i++)
-            {
-                Tuple<int, double> point = pvList[i];
-                Tuple<int, double> next = new Tuple<int, double>(-1, -1.0);
-                // If i is not the last element, set next to proceeding element.
-                if (i + 1 < pvList.Count)
-                {
-                    next = pvList[i + 1];
-                }
-                // If there are indeed previous and next elements,
-                if (prev.Item2 != -1.0 && next.Item2 != -1.0)
-                {
-                    // If a data point is in the middle of the two on either side, remove it.
-                    if ((point.Item2 > prev.Item2 &&
-                         point.Item2 < next.Item2) ||
-                        (point.Item2 < prev.Item2 &&
-                         point.Item2 > next.Item2))
-                    {
-                        pvList.RemoveAt(i);
-                        i--;
-                        continue;
-                    }
-                }
-
-                // Set previous to current node.
-                prev = pvList[i];
-            }
-        }
-
-        //--------------------------------------------------------------------------------
-        /// <summary>
         /// Calculates the periods in a given graph wave based on its peaks and valleys
         /// </summary>
         /// <param name="wave">Data points that make the core of the wave</param>
@@ -150,7 +111,7 @@ namespace CaptureUtil.Algorithms
         /// <returns>The amount of periods found within the wave</returns>
         public int CalculateThePeriod(List<double> wave, 
                                       List<Tuple<int, double>> pv, 
-                                      bool clearInconsistantPeriods = true, 
+                                      bool clearInconsistantPeriods = false, 
                                       int clearReturnTheshold = 3)
         {
             var pvPairs                     = GetPVPairs(pv,wave);
@@ -239,18 +200,19 @@ namespace CaptureUtil.Algorithms
         private List<Tuple<int, int, int>> GetPVPairs(List<Tuple<int, double>> pv, 
                                                       List<double> wave)
         {
-            var pvPairs = new List<Tuple<int,int,int>>();
-            int i = 0;
-
             if (pv == null || pv.Count < 2)
             {
                 return null;
             }
 
-            //Check to see if the frist index is a peak or valley, skip if it's peak.
+            var pvPairs = new List<Tuple<int,int,int>>();
+            int i = 0;
+            bool pvpPattern = pv[0].Item2 > pv[1].Item2;
+
+            //Check to see if the first index is a peak or valley, skip if it's peak.
             //--->This is a quick and dirty way of having valid pattern (VPV).
-            if (pv[i].Item2 > pv[i + 1].Item2)
-                i++;
+//            if (pv[i].Item2 > pv[i + 1].Item2)
+//                i++;
 
             //Gather all pairs of Valley_Peak_Valley's in the PV list
             for (; i < pv.Count; i += 2)
@@ -264,13 +226,14 @@ namespace CaptureUtil.Algorithms
                 }
                 else if(i + 1 < pv.Count)
                 {
-                    var subArray      = wave.Skip(pv[i + 1].Item1).ToArray();
-                    var smallestIndex = SMath.FindSmallestValueIndex(subArray) + 
-                                        pv[i + 1].Item1;
+                    var subArray = wave.Skip(pv[i + 1].Item1).ToArray();
+                    var newIndex = (pvpPattern ? SMath.FindLargestValueIndex(subArray) 
+                                              : SMath.FindSmallestValueIndex(subArray))
+                                              + pv[i + 1].Item1;
 
                     var tuple2 = new Tuple<int, int, int>(pv[i].Item1,
                                                           pv[i + 1].Item1,
-                                                          smallestIndex);
+                                                          newIndex);
                     pvPairs.Add(tuple2);
                 }
             }
@@ -331,6 +294,8 @@ namespace CaptureUtil.Algorithms
             int indexEnd      = 0;              //Ending point 2nd peak valley
             int passedMidline = 0;              //Amount of times passed a midpoint
             int frequency     = 0;              //index counter, after passing midpoints
+            bool vpvRoutine   = wave[pvPair1.Item1] < wave[pvPair1.Item2];
+            bool mlReached    = false;
 
             //Check to see if we are going to calculate the end of the wave 
             // instead of two peak points.
@@ -348,24 +313,37 @@ namespace CaptureUtil.Algorithms
             {
                 switch (passedMidline)
                 {
-                    case 0: //We are at the left side valley, going up to the peak
-                        if (SMath.IntervalRange(wave[index], wave[index + 1], stMidline))
+                    case 0:
+                        mlReached = vpvRoutine
+                            ? SMath.IntervalRange(wave[index], wave[index + 1], stMidline)
+                            : SMath.IntervalRange(wave[index + 1],wave[index], stMidline);
+
+                        if (mlReached)
+                        {
+
+                                passedMidline++;
+                        }
+                        break;
+                    case 1:
+                        mlReached = vpvRoutine
+                            ? SMath.IntervalRange(wave[index + 1],wave[index],stMidline)
+                            : SMath.IntervalRange(wave[index], wave[index + 1],stMidline);
+
+                        if (mlReached)
                         {
                             passedMidline++;
                         }
+
                         break;
-                    case 1: //We are at the peak going down to the right side valley
-                        if (SMath.IntervalRange(wave[index + 1], wave[index], stMidline))
-                        {
-                            passedMidline++;
-                        }
-                        break;
-                    default://We are at the valley going to the next peak
+                    default:
                         if (pvPair2 != null && index >= pvPair2.Item1)
                         {
-                            if (SMath.IntervalRange(wave[index],
-                                                    wave[index + 1],
-                                                    enMidline))
+                            mlReached = vpvRoutine
+                                ? SMath.IntervalRange(wave[index],wave[index + 1],
+                                                      enMidline)
+                                : SMath.IntervalRange(wave[index + 1], wave[index],
+                                                      enMidline);
+                            if (mlReached)
                             {
                                 passedMidline++;
                             }
