d6e8299b0c3992f54745fc64924713d9aee9b9f5
George Karaszi <georgekaraszi@gmail.com>
2015-11-08 17:46:44 -0500

    Fixed the VPV pattern issue. This algorithm now accepts PVP and VPV patterns.

--- Source/Algorithms/PeaksAndValleys.cs
+++ Source/Algorithms/PeaksAndValleys.cs
@@ -207,7 +207,6 @@ namespace WesternMichgian.SeniorDesign.KinectProject.Algorithms
 
             return period;
         }
-
         //--------------------------------------------------------------------------------
         /// <summary>
         /// Build a list of index's that are pairs of Valley, Peak, Valley sets
@@ -220,41 +219,43 @@ namespace WesternMichgian.SeniorDesign.KinectProject.Algorithms
         /// Item2: Peak index
         /// Item3: Right side valley point
         /// </returns>
-        private List<Tuple<int, int, int>> GetPVPairs(List<Tuple<int, double>> pv, 
+        private List<Tuple<int, int, int>> GetPVPairs(List<Tuple<int, double>> pv,
                                                       List<double> wave)
         {
-            var pvPairs = new List<Tuple<int,int,int>>();
-            int i = 0;
-
             if (pv == null || pv.Count < 2)
             {
                 return null;
             }
 
-            //Check to see if the frist index is a peak or valley, skip if it's peak.
+            var pvPairs = new List<Tuple<int, int, int>>();
+            int i = 0;
+            bool pvpPattern = pv[0].Item2 > pv[1].Item2;
+
+            //Check to see if the first index is a peak or valley, skip if it's peak.
             //--->This is a quick and dirty way of having valid pattern (VPV).
-            if (pv[i].Item2 > pv[i + 1].Item2)
-                i++;
+            //            if (pv[i].Item2 > pv[i + 1].Item2)
+            //                i++;
 
             //Gather all pairs of Valley_Peak_Valley's in the PV list
             for (; i < pv.Count; i += 2)
             {
                 if (i + 2 < pv.Count)
                 {
-                    var tuple3 = new Tuple<int,int,int>(pv[i].Item1, 
-                                                        pv[i+1].Item1, 
-                                                        pv[i+2].Item1);
+                    var tuple3 = new Tuple<int, int, int>(pv[i].Item1,
+                                                        pv[i + 1].Item1,
+                                                        pv[i + 2].Item1);
                     pvPairs.Add(tuple3);
                 }
-                else if(i + 1 < pv.Count)
+                else if (i + 1 < pv.Count)
                 {
-                    var subArray      = wave.Skip(pv[i + 1].Item1).ToArray();
-                    var smallestIndex = SMath.FindSmallestValueIndex(subArray) + 
-                                        pv[i + 1].Item1;
+                    var subArray = wave.Skip(pv[i + 1].Item1).ToArray();
+                    var newIndex = (pvpPattern ? SMath.FindLargestValueIndex(subArray)
+                                              : SMath.FindSmallestValueIndex(subArray))
+                                              + pv[i + 1].Item1;
 
                     var tuple2 = new Tuple<int, int, int>(pv[i].Item1,
                                                           pv[i + 1].Item1,
-                                                          smallestIndex);
+                                                          newIndex);
                     pvPairs.Add(tuple2);
                 }
             }
@@ -278,12 +279,12 @@ namespace WesternMichgian.SeniorDesign.KinectProject.Algorithms
         private Tuple<double, double> MidlineAmp(Tuple<int, int, int> focus,
                                                   List<double> wave)
         {
-            double valleyAverage = ( wave[focus.Item1] + wave[focus.Item3] )/2;
-            double midline       = (wave[focus.Item2] + valleyAverage) / 2;
-            double amplitude     = Math.Abs(wave[focus.Item2] - midline);
+            double valleyAverage = (wave[focus.Item1] + wave[focus.Item3]) / 2;
+            double midline = (wave[focus.Item2] + valleyAverage) / 2;
+            double amplitude = Math.Abs(wave[focus.Item2] - midline);
 
 
-            return new Tuple<double,double>(midline,amplitude);
+            return new Tuple<double, double>(midline, amplitude);
         }
 
         //--------------------------------------------------------------------------------
@@ -311,20 +312,22 @@ namespace WesternMichgian.SeniorDesign.KinectProject.Algorithms
                                      Tuple<int, int, int> pvPair2,
                                      List<double> wave)
         {
-            int index         = pvPair1.Item1;  //Starting base valley index (left side)
-            int indexEnd      = 0;              //Ending point 2nd peak valley
+            int index = pvPair1.Item1;  //Starting base valley index (left side)
+            int indexEnd = 0;              //Ending point 2nd peak valley
             int passedMidline = 0;              //Amount of times passed a midpoint
-            int frequency     = 0;              //index counter, after passing midpoints
+            int frequency = 0;              //index counter, after passing midpoints
+            bool vpvRoutine = wave[pvPair1.Item1] < wave[pvPair1.Item2];
+            bool mlReached = false;
 
             //Check to see if we are going to calculate the end of the wave 
             // instead of two peak points.
             if (pvPair1.Item3 == -1 || pvPair2 == null)
             {
-                indexEnd      = wave.Count - 1;
+                indexEnd = wave.Count - 1;
             }
             else
             {
-                indexEnd      = pvPair2.Item2;
+                indexEnd = pvPair2.Item2;
             }
 
 
@@ -332,24 +335,37 @@ namespace WesternMichgian.SeniorDesign.KinectProject.Algorithms
             {
                 switch (passedMidline)
                 {
-                    case 0: //We are at the left side valley, going up to the peak
-                        if (SMath.IntervalRange(wave[index], wave[index + 1], stMidline))
+                    case 0:
+                        mlReached = vpvRoutine
+                            ? SMath.IntervalRange(wave[index], wave[index + 1], stMidline)
+                            : SMath.IntervalRange(wave[index + 1], wave[index], stMidline);
+
+                        if (mlReached)
                         {
+
                             passedMidline++;
                         }
                         break;
-                    case 1: //We are at the peak going down to the right side valley
-                        if (SMath.IntervalRange(wave[index + 1], wave[index], stMidline))
+                    case 1:
+                        mlReached = vpvRoutine
+                            ? SMath.IntervalRange(wave[index + 1], wave[index], stMidline)
+                            : SMath.IntervalRange(wave[index], wave[index + 1], stMidline);
+
+                        if (mlReached)
                         {
                             passedMidline++;
                         }
+
                         break;
-                    default://We are at the valley going to the next peak
+                    default:
                         if (pvPair2 != null && index >= pvPair2.Item1)
                         {
-                            if (SMath.IntervalRange(wave[index],
-                                                    wave[index + 1],
-                                                    enMidline))
+                            mlReached = vpvRoutine
+                                ? SMath.IntervalRange(wave[index], wave[index + 1],
+                                                      enMidline)
+                                : SMath.IntervalRange(wave[index + 1], wave[index],
+                                                      enMidline);
+                            if (mlReached)
                             {
                                 passedMidline++;
                             }
@@ -360,12 +376,12 @@ namespace WesternMichgian.SeniorDesign.KinectProject.Algorithms
                 if (passedMidline > 0)
                     frequency++;
 
-            } while (index++ < indexEnd 
-                    && passedMidline < 3 
-                    && (index+1) < wave.Count);
+            } while (index++ < indexEnd
+                    && passedMidline < 3
+                    && (index + 1) < wave.Count);
 
             //if we passed or reached the mid lines, return frequency, otherwise 0
-           return (passedMidline) == 3 ? frequency:0;
+            return (passedMidline) == 3 ? frequency : 0;
         }
     }
 }
