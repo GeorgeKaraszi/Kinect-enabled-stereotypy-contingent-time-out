ffc4c739a07065926f62ac647135ca3a5e16f837
George Karaszi <georgekaraszi@gmail.com>
2015-11-14 14:33:43 -0500

    Cleaned up and reformatted Matt's code to better fit the coding standards. As well as removed and slightly modified redundancies and performance concerns.

--- Source/Algorithms/GestureInterpreter.cs
+++ Source/Algorithms/GestureInterpreter.cs
@@ -1,156 +1,156 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WesternMichgian.SeniorDesign.KinectProject.Algorithms
 {
     public class GestureInterpreter
     {
         // Circular array implementation of wave buffer.
-        private double[] wave;
+        private readonly double[] _wave;
 
         // List of peaks and valleys.
-        LinkedList<PeakValley> PVList;
+        readonly List<PeakValley> _pvList;
         // List of PVPairs (VPV or PVP) for calculating frequency.
-        LinkedList<PVPair> PVPairs;
+        readonly List<PvPair> _pvPairs;
         // List of periods. It's a list so it can be decremented as PVPairs are removed.
-        LinkedList<int> periods;
+        readonly List<int> _periods;
 
         // Size of wave buffer to analyze.
-        private const int window_size = 60;
+        private const int WindowSize      = 60;
 
         // Minimum and maximum frequency.
-        private const int min_frequency = 5;
-        private const int max_frequency = 26;
+        private const int MinFrequency    = 5;
+        private const int MaxFrequency    = 26;
+
+        //Minimum value in which triggers the event
+        private const int PeriodThreshold = 3;
 
         // Minimum distance between peaks and valleys.
-        private const double min_peak_valley_distance = 0.15;
+        private const double MinPeakValleyDistance = 0.15;
 
         // Number of frames for counting.
-        int frame;
+        private int Frame { get; set; }
 
         // Peak and valley data initialization.
-        double peak;
-        double valley;
+        private double Peak { get; set; }
+        private double Valley { get; set; }
         // Frames at which the peak and valley is set.
-        int peakset;
-        int valset;
+        private int Peakset { get; set; }
+        private int Valset { get; set; }
 
+        //--------------------------------------------------------------------------------
         /// <summary>
         /// Initializes all necessary data members of the GestureInterpreter class.
         /// </summary>
         public GestureInterpreter()
         {
-            wave = new double[window_size];
+            _wave    = new double[WindowSize];
 
-            PVList = new LinkedList<PeakValley>();
-            PVPairs = new LinkedList<PVPair>();
-            periods = new LinkedList<int>();
+            _pvList  = new List<PeakValley>();
+            _pvPairs = new List<PvPair>();
+            _periods = new List<int>();
 
-            frame = -1;
+            Frame    = -1;
 
-            peak = 0;
-            valley = 1;
+            Peak     = 0;
+            Valley   = 1;
 
-            peakset = -1;
-            valset = -1;
+            Peakset  = -1;
+            Valset   = -1;
         }
 
+        //--------------------------------------------------------------------------------
         /// <summary>
         /// Process input and return if hand flap has been detected.
         /// </summary>
         /// <param name="val"> Double value from gesture detector. </param>
-        /// <returns> A boolean of whether sufficient hand-flapping has been detected or not. </returns>
+        /// <returns> 
+        /// A boolean of whether sufficient hand-flapping has been detected or not. 
+        /// </returns>
         public bool ProcessPoint(double val)
         {
             bool returnVal = false;
-
-            frame++;
+            int index      = ++Frame % WindowSize;
+            _wave[index]   = val;
 
             // First, check if there is to be a PV removed from the list.
-            if (PVList.Count > 0 && PVList.First().frame <= frame - window_size)
+            if (_pvList.Count > 0 && _pvList.First().Frame <= Frame - WindowSize)
             {
-                //System.Console.WriteLine("Removing PV {0}, {1}...", PVList.First().frame, PVList.First().value);
-                PVList.RemoveFirst();
+                _pvList.RemoveAt(0);
             }
 
             // And if there is a PVPair to be removed from the list.
-            if (PVPairs.Count > 0 && PVPairs.First().frames.Item1 <= frame - window_size)
+            if (_pvPairs.Count > 0 && _pvPairs.First().Frames.Item1 <= Frame - WindowSize)
             {
-                PVPair pvp = PVPairs.First();
-                //System.Console.WriteLine("Removing PVPair {0}, {1}, {2}...", pvp.frames.Item1, pvp.frames.Item2, pvp.frames.Item3);
+                PvPair pvp = _pvPairs.First();
+
                 // If the removal of this PVPair entitles the removal of a period.
-                if (periods.Count > 0 && pvp.frames.Item1 == periods.First())
+                if (_periods.Count > 0 && pvp.Frames.Item1 == _periods.First())
                 {
-                    periods.RemoveFirst();
-                    //System.Console.WriteLine("Period is now {0}.", periods.Count);
+                    _periods.RemoveAt(0);
                 }
-                PVPairs.RemoveFirst();
+                _pvPairs.RemoveAt(0);
             }
 
-            int index = frame % window_size;
-            wave[index] = val;
-            //System.Console.WriteLine("wave[{0}] = {1}.", index, val);
-
             // If the point is between peak and valley
-            if (wave[index] <= peak && wave[index] >= valley)
+            if (_wave[index] <= Peak && _wave[index] >= Valley)
             {
-                if (peak - valley < min_peak_valley_distance)
+                if (Peak - Valley < MinPeakValleyDistance)
                 {
                     return false;
                 }
                 // If valset is oldest, save valley to list, reset valley, and continue.
-                if (valset < peakset)
+                if (Valset < Peakset)
                 {
                     //Be sure the valley is less then the last captured peak.
-                    if (PVList.Count > 0 && valley >= PVList.Last().value)
+                    if (_pvList.Count > 0 && Valley >= _pvList.Last().Value)
                     {
-                        valset = frame;
-                        valley = wave[index];
+                        Valset = Frame;
+                        Valley = _wave[index];
                         return false;
                     }
 
-                    // If Adding a PeakValley triggers hand-flapping detection, return true.
-                    returnVal = this.AddPV(new PeakValley(valset, valley));
+                    //If Adding a PeakValley triggers hand-flapping detection, return true.
+                    returnVal = AddPv(new PeakValley(Valset, Valley));
 
-                    valley = 1;
+                    Valley = 1;
                     return returnVal;
                 }
 
                 // If peakset is oldest, save peak to list, reset peak, and continue.
-                if (peakset < valset)
+                if (Peakset < Valset)
                 {
                     //Be sure the peak is higher then the last captured valley
-                    if (PVList.Count > 0 && peak <= PVList.Last().value)
+                    if (_pvList.Count > 0 && Peak <= _pvList.Last().Value)
                     {
-                        peakset = frame;
-                        peak = wave[index];
+                        Peakset = Frame;
+                        Peak = _wave[index];
                         return false;
                     }
 
-                    // If Adding a PeakValley triggers hand-flapping detection, return true.
-                    returnVal = this.AddPV(new PeakValley(peakset, peak));
+                    //If Adding a PeakValley triggers hand-flapping detection, return true.
+                    returnVal = AddPv(new PeakValley(Peakset, Peak));
 
-                    peak = 0;
+                    Peak = 0;
                     return returnVal;
                 }
             }
-            if (wave[index] > peak)
+            if (_wave[index] > Peak)
             {
-                peakset = frame;
-                peak = wave[index];
+                Peakset = Frame;
+                Peak = _wave[index];
             }
-            if (wave[index] < valley)
+            if (_wave[index] < Valley)
             {
-                valset = frame;
-                valley = wave[index];
+                Valset = Frame;
+                Valley = _wave[index];
             }
 
-            return returnVal;
+            return false;
         }
 
+        //--------------------------------------------------------------------------------
         /// <summary>
         /// Add a peak or a valley (a PV) to the list of PVs.
         /// </summary>
@@ -159,132 +159,164 @@ namespace WesternMichgian.SeniorDesign.KinectProject.Algorithms
         /// If sufficient hand-flapping is detected in adding this PV,
         /// return true, otherwise return false.
         /// </returns>
-        private bool AddPV(PeakValley pv)
+        private bool AddPv(PeakValley pv)
         {
-            PVList.AddLast(pv);
-            //Console.WriteLine("Added new PV: {0}, {1}.", pv.frame, pv.value);
+            Tuple<double, double> midAmp = null;
+            int[] pvGroup                = null;
 
-            // If there are at least three points and the last point is not the third point in the last PVPair
+            _pvList.Add(pv);
+
+            // If there are at least three points and the last point is not the third 
+            // point in the last PVPair
             // (if there is one), then create a PVPair from these three points.
-            if (PVList.Count > 2)
+            if (_pvList.Count <= 2)
+                return false;
+
+            if (_pvPairs.Count > 0 && 
+                _pvPairs.Last().Frames.Item3 == _pvList[_pvList.Count - 2].Frame)
             {
-                if (PVPairs.Count > 0 && PVPairs.Last().frames.Item3 == PVList.ElementAt<PeakValley>(PVList.Count - 2).frame)
-                {
+                return false;
+            }
+
+            //Gather a group of indexes that form the wave
+            pvGroup = _pvList.Skip(_pvList.Count - 4).Select(s => s.Frame).ToArray();
+            midAmp  = MidlineAmp(pvGroup);
+
+            _pvPairs.Add(new PvPair(pvGroup, midAmp));
+
+            //Increment the period if the frequency threshold is met
+            if (_pvPairs.Count > 1)
+            {
+                PvPair prevpvp = _pvPairs[_pvPairs.Count - 2];
+                int frequency = FrequencyBetween(prevpvp, _pvPairs.Last());
+
+                if (frequency <= MinFrequency || frequency >= MaxFrequency)
                     return false;
-                }
 
-                int n = PVList.Count;
-                int p1 = PVList.ElementAt<PeakValley>(n - 3).frame;
-                int p2 = PVList.ElementAt<PeakValley>(n - 2).frame;
-                int p3 = PVList.ElementAt<PeakValley>(n - 1).frame;
-                double outsideAverage = (wave[p1 % window_size] + wave[p3 % window_size]) / 2;
-                double midline = (wave[p2 % window_size] + outsideAverage) / 2;
-                double amplitude = Math.Abs(wave[p2 % window_size] - midline);
-
-                PVPairs.AddLast(new PVPair(new Tuple<int, int, int>(p1, p2, p3), midline, amplitude));
-                //Console.WriteLine("Created a new PVPair, {0}, {1}, {2} with midline {3} and amplitude {4}.", p1, p2, p3, midline, amplitude);
-
-                // Now, if there is a previous PVPair, get the frequency between the two PVPairs.
-                // If the frequency is greater than the threshold minimum and less than the
-                // threshold maximum, increment the period.
-                if (PVPairs.Count > 1)
+                _periods.Add(_pvPairs.Last().Frames.Item1);
+                if (_periods.Count >= PeriodThreshold)
                 {
-                    PVPair prevpvp = PVPairs.ElementAt<PVPair>(PVPairs.Count - 2);
-                    int frequency = this.FrequencyBetween(prevpvp, PVPairs.Last());
-                    if (frequency > min_frequency && frequency < max_frequency)
-                    {
-                        periods.AddLast(PVPairs.Last().frames.Item1);
-                        //System.Console.WriteLine("Frequency = {0} and Period = {1}.", frequency, periods.Count);
-                        if (periods.Count >= 3)
-                        {
-                            return true;
-                        }
-                    }
+                    return true;
                 }
             }
 
             return false;
         }
 
+        //--------------------------------------------------------------------------------
+        /// <summary>
+        /// Calculates the mid line and amplitude of a given set of valley and peaks
+        /// </summary>
+        /// <param name="pvGroup">
+        /// Group of indexes that form the peak and valley of the given wave
+        /// </param>
+        /// <returns>
+        /// Tuple containing 
+        /// Item1: mid line between peak and valley
+        /// Item2: amplitude between peak and valley
+        /// </returns>
+        private Tuple<double, double> MidlineAmp(IReadOnlyList<int> pvGroup )
+        {
+            int p1                = pvGroup[0] % WindowSize;
+            int p2                = pvGroup[1] % WindowSize;
+            int p3                = pvGroup[2] % WindowSize;
+
+            double outsideAverage = (_wave[p1] + _wave[p3]) / 2;
+            double midline        = (_wave[p2] + outsideAverage) / 2;
+            double amplitude      = Math.Abs(_wave[p2] - midline);
+
+            return new Tuple<double, double>(midline, amplitude);
+        }
+
+        //--------------------------------------------------------------------------------
         /// <summary>
         /// Calculate the frequency between two PVPairs.
         /// </summary>
         /// <param name="pvp1"> First PVPair whose midline is being used. </param>
         /// <param name="pvp2"> Second PVPair. </param>
         /// <returns> The number of frames comprising a period of the wave. </returns>
-        private int FrequencyBetween(PVPair pvp1, PVPair pvp2)
+        private int FrequencyBetween(PvPair pvp1, PvPair pvp2)
         {
-            int index;
-            bool passedMidLine;
-            int start;
-            int end;
-            // 1 for VPV and 0 for PVP.
-            bool pvpattern = wave[pvp1.frames.Item1 % window_size] < wave[pvp1.frames.Item2 % window_size];
+            int index          = pvp1.Frames.Item1; // Get start Frame for calculation.
+            bool passedMidLine = false;
+            int start          = 0;
+            int end            = 0;
 
-            // Get start frame for frequency calculation.
-            index = pvp1.frames.Item1;
-            passedMidLine = false;
+            // 1 for VPV and 0 for PVP.
+            bool pvpattern     = _wave[pvp1.Frames.Item1 % WindowSize] < 
+                                 _wave[pvp1.Frames.Item2 % WindowSize];
+
             while (passedMidLine == false)
             {
-                int i = index % window_size;
-                int j = (index + 1) % window_size;
+                int i = index++ % WindowSize;
+                int j = index % WindowSize;
                 passedMidLine =
-                    pvpattern ? (pvp1.midline > wave[i] && pvp1.midline < wave[j] ? true : false)
-                              : (pvp1.midline < wave[i] && pvp1.midline > wave[j] ? true : false);
-
-                index++;
+                    pvpattern ? (pvp1.Midline > _wave[i] && pvp1.Midline < _wave[j])
+                              : (pvp1.Midline < _wave[i] && pvp1.Midline > _wave[j]);
             }
             start = index;
 
-            // Get end frame for frequency calculation.
-            index = pvp2.frames.Item1;
+            // Get end Frame for frequency calculation.
+            index = pvp2.Frames.Item1;
             passedMidLine = false;
             while (passedMidLine == false)
             {
-                int i = index % window_size;
-                int j = (index + 1) % window_size;
+                int i = index++ % WindowSize;
+                int j = index % WindowSize;
                 passedMidLine =
-                    pvpattern ? (pvp1.midline > wave[i] && pvp1.midline < wave[j] ? true : false)
-                              : (pvp1.midline < wave[i] && pvp1.midline > wave[j] ? true : false);
-
-                index++;
+                    pvpattern ? (pvp1.Midline > _wave[i] && pvp1.Midline < _wave[j])
+                              : (pvp1.Midline < _wave[i] && pvp1.Midline > _wave[j]);
             }
             end = index;
 
             return end - start;
         }
+
+        //================================================================================
         /// <summary>
         /// Class for objects representing either peaks or valleys.
-        /// Data members are the frame identifying number and the progress value.
+        /// Data members are the Frame identifying number and the progress value.
         /// </summary>
         private class PeakValley
         {
-            public int frame { get; }
-            public double value { get; }
+            public int Frame { get; }
+            public double Value { get; }
 
+            //----------------------------------------------------------------------------
             public PeakValley(int frame, double value)
             {
-                this.frame = frame;
-                this.value = value;
+                Frame = frame;
+                Value = value;
             }
         }
 
+        //================================================================================
         /// <summary>
         /// Class for objects representing data points of the form VPV or PVP.
-        /// Data members are the three frame numbers, the midline, and the amplitude
+        /// Data members are the three Frame numbers, the midline, and the amplitude
         /// </summary>
-        public class PVPair
+        private class PvPair
         {
             // Item1, 2, and 3 correspond to the frames of the PVPair.
-            public Tuple<int, int, int> frames { get; }
-            public double midline { get; }
-            public double amplitude { get; }
+            public Tuple<int, int, int> Frames { get; }
+            public double Midline { get; }
+            public double Amplitude { get; }
 
-            public PVPair(Tuple<int, int, int> frames, double midline, double amplitude)
+            //----------------------------------------------------------------------------
+            public PvPair(Tuple<int, int, int> frames, double midline, double amplitude)
             {
-                this.frames = new Tuple<int, int, int>(frames.Item1, frames.Item2, frames.Item3);
-                this.midline = midline;
-                this.amplitude = amplitude;
+                Frames    = new Tuple<int, int, int>(frames.Item1, frames.Item2, 
+                                                     frames.Item3);
+                Midline   = midline;
+                Amplitude = amplitude;
+            }
+
+            //----------------------------------------------------------------------------
+            public PvPair(IReadOnlyList<int> frames, Tuple<double, double> midAmp)
+            {
+                Frames    = new Tuple<int, int, int>(frames[0],frames[1],frames[2]);
+                Midline   = midAmp.Item1;
+                Amplitude = midAmp.Item2;
             }
         }
     }
