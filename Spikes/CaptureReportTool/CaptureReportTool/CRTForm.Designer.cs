6d5dfa8d4fc2a58dbc6af12d3c942c90bfbd41f9
George Karaszi <georgekaraszi@gmail.com>
2015-11-28 16:07:01 -0500

    Tool extension spike for the main program. This designed to invoke events that control the actions and results of the GUI for analysis. This tool has been scaled to handle kinects 6 skeletal body tracking too.

---
+++ Spikes/CaptureReportTool/CaptureReportTool/CRTForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace CaptureReportTool
+{
+    partial class CRTForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.activeTracking = new System.Windows.Forms.NumericUpDown();
+            this.trackBtn = new System.Windows.Forms.Button();
+            this.openWndBtn = new System.Windows.Forms.Button();
+            this.rmv_topbtn = new System.Windows.Forms.Button();
+            this.rmv_midbtn = new System.Windows.Forms.Button();
+            this.rmv_btmbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gestureLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.activeTracking)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // activeTracking
+            // 
+            this.activeTracking.Location = new System.Drawing.Point(62, 69);
+            this.activeTracking.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.activeTracking.Name = "activeTracking";
+            this.activeTracking.Size = new System.Drawing.Size(105, 20);
+            this.activeTracking.TabIndex = 0;
+            this.activeTracking.ValueChanged += new System.EventHandler(this.activeTracking_ValueChanged);
+            // 
+            // trackBtn
+            // 
+            this.trackBtn.Location = new System.Drawing.Point(74, 95);
+            this.trackBtn.Name = "trackBtn";
+            this.trackBtn.Size = new System.Drawing.Size(75, 23);
+            this.trackBtn.TabIndex = 1;
+            this.trackBtn.Text = "Track";
+            this.trackBtn.UseVisualStyleBackColor = true;
+            this.trackBtn.Click += new System.EventHandler(this.trackBtn_Click);
+            // 
+            // openWndBtn
+            // 
+            this.openWndBtn.Location = new System.Drawing.Point(62, 13);
+            this.openWndBtn.Name = "openWndBtn";
+            this.openWndBtn.Size = new System.Drawing.Size(109, 23);
+            this.openWndBtn.TabIndex = 2;
+            this.openWndBtn.Text = "Open Tool Window";
+            this.openWndBtn.UseVisualStyleBackColor = true;
+            this.openWndBtn.Click += new System.EventHandler(this.openWndBtn_Click);
+            // 
+            // rmv_topbtn
+            // 
+            this.rmv_topbtn.Location = new System.Drawing.Point(55, 158);
+            this.rmv_topbtn.Name = "rmv_topbtn";
+            this.rmv_topbtn.Size = new System.Drawing.Size(112, 23);
+            this.rmv_topbtn.TabIndex = 3;
+            this.rmv_topbtn.Text = "Remove Top";
+            this.rmv_topbtn.UseVisualStyleBackColor = true;
+            this.rmv_topbtn.Click += new System.EventHandler(this.rmv_topbtn_Click);
+            // 
+            // rmv_midbtn
+            // 
+            this.rmv_midbtn.Location = new System.Drawing.Point(55, 187);
+            this.rmv_midbtn.Name = "rmv_midbtn";
+            this.rmv_midbtn.Size = new System.Drawing.Size(112, 23);
+            this.rmv_midbtn.TabIndex = 4;
+            this.rmv_midbtn.Text = "Remove Middle";
+            this.rmv_midbtn.UseVisualStyleBackColor = true;
+            this.rmv_midbtn.Click += new System.EventHandler(this.rmv_midbtn_Click);
+            // 
+            // rmv_btmbtn
+            // 
+            this.rmv_btmbtn.Location = new System.Drawing.Point(55, 216);
+            this.rmv_btmbtn.Name = "rmv_btmbtn";
+            this.rmv_btmbtn.Size = new System.Drawing.Size(112, 23);
+            this.rmv_btmbtn.TabIndex = 5;
+            this.rmv_btmbtn.Text = "Remove Bottom";
+            this.rmv_btmbtn.UseVisualStyleBackColor = true;
+            this.rmv_btmbtn.Click += new System.EventHandler(this.rmv_btmbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(178, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Target Gesture Event:";
+            // 
+            // gestureLbl
+            // 
+            this.gestureLbl.AutoSize = true;
+            this.gestureLbl.Location = new System.Drawing.Point(297, 105);
+            this.gestureLbl.Name = "gestureLbl";
+            this.gestureLbl.Size = new System.Drawing.Size(102, 13);
+            this.gestureLbl.TabIndex = 7;
+            this.gestureLbl.Text = "(#id):(GestureName)";
+            // 
+            // CRTForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 266);
+            this.Controls.Add(this.gestureLbl);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rmv_btmbtn);
+            this.Controls.Add(this.rmv_midbtn);
+            this.Controls.Add(this.rmv_topbtn);
+            this.Controls.Add(this.openWndBtn);
+            this.Controls.Add(this.trackBtn);
+            this.Controls.Add(this.activeTracking);
+            this.Name = "CRTForm";
+            this.Text = "Main Window";
+            ((System.ComponentModel.ISupportInitialize)(this.activeTracking)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown activeTracking;
+        private System.Windows.Forms.Button trackBtn;
+        private System.Windows.Forms.Button openWndBtn;
+        private System.Windows.Forms.Button rmv_topbtn;
+        private System.Windows.Forms.Button rmv_midbtn;
+        private System.Windows.Forms.Button rmv_btmbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label gestureLbl;
+    }
+}
+
---
+++ Spikes/CaptureReportTool/CaptureReportTool/UtilWindow.cs
@@ -0,0 +1,354 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace CaptureReportTool
+{
+    public partial class UtilWindow : Form
+    {
+        public event ChangeInTargetGestureEvent OnGestureTargetChange;
+
+        /// <summary>
+        /// List of available gestures that can be targeted
+        /// </summary>
+        private string[] GestureList { get; set; }
+        /// <summary>
+        /// List of panels that can be displayed for tracked bodies
+        /// </summary>
+        private List<PanelFrame> ActivePanel { get; set; }
+        /// <summary>
+        /// List of recording tables of tracked bodies
+        /// </summary>
+        private Hashtable HashTblRecord { get; set; }
+        /// <summary>
+        /// Window size of the displayed graph
+        /// </summary>
+        private int WindowSize { get; } = 100;
+
+        //--------------------------------------------------------------------------------
+        public UtilWindow()
+        {
+            InitializeComponent();
+            SetupPanels();
+        }
+
+        public UtilWindow(string[] gestureNames)
+        {
+            InitializeComponent();
+            GestureList = gestureNames;
+            SetupPanels();
+        }
+
+        //--------------------------------------------------------------------------------
+        /// <summary>
+        /// Initializes the list of tracked panels being displayed.
+        /// </summary>
+        private void SetupPanels()
+        {
+            HashTblRecord = new Hashtable();
+            ActivePanel = new List<PanelFrame>
+                          {
+                              new PanelFrame(false,false, -1, panel1),
+                              new PanelFrame(false,false, -1, panel2),
+                              new PanelFrame(false,false, -1, panel3),
+                              new PanelFrame(false,false, -1, panel4),
+                              new PanelFrame(false,false, -1, panel5),
+                              new PanelFrame(false,false, -1, panel6)
+                          };
+        }
+
+        //--------------------------------------------------------------------------------
+        /// <summary>
+        /// In the case of a skeleton is no longer tracked between two active one's. 
+        /// The GUI must rearrange the GUI in order to prevent gaps.
+        /// 
+        /// Due to the nature of the application. A simple swap of panels would cause 
+        /// error's and a higher level of complexity since each panel needs to be 
+        /// tracked individually.
+        /// 
+        /// This function does a 1 time pass through that swaps all data from an active to 
+        /// an inactive panel that is located above.
+        /// </summary>
+        private void RearrangePanels()
+        {
+            PanelFrame above;
+            PanelFrame below;
+            Chart chartTop = null;
+            Chart chartBtm = null;
+            for (int i = 0; ( i + 1 ) < ActivePanel.Count; i++)
+            {
+                above = ActivePanel[i];
+                below = ActivePanel[i + 1];
+
+                if (above.IsActive == false && below.IsActive)
+                {
+                    above.IsActive      = below.IsActive;
+                    above.IsRecording   = below.IsRecording;
+                    above.BodyId        = below.BodyId;
+                    above.Panel.Visible = below.Panel.Visible;
+                    below.Panel.Visible = false;
+                    below.IsActive      = false;
+                    below.BodyId        = -1;
+                    var topPanel        = above.Panel.Controls;
+                    var btmPanel        = below.Panel.Controls;
+
+                    //Obtain chart object for top panel
+                    foreach (var con in topPanel.Cast<object>()
+                                                .Where( con =>
+                                                        con.GetType() == typeof (Chart)))
+                    {
+                        chartTop = (Chart) con;
+                    }
+
+                    //Obtain chart object for bottom panel
+                    foreach (var con in btmPanel.Cast<object>()
+                                                .Where( con =>
+                                                        con.GetType() == typeof (Chart)))
+                    {
+                        chartBtm = (Chart) con;
+                    }
+
+                    //Make sure these values are not null
+                    if (chartTop == null || chartBtm == null)
+                        continue;
+
+                    //Swap chart display's
+                    chartTop.Series[0].Points.Clear();
+                    foreach (var point in chartBtm.Series[0].Points)
+                    {
+                        chartTop.Series[0].Points.Add(point);
+                    }
+                }
+            }
+        }
+
+        //--------------------------------------------------------------------------------
+        /// <summary>
+        /// Records inserted data if the target skeleton is marked for recording
+        /// </summary>
+        /// <param name="bodyid">ID of targeted skeleton</param>
+        /// <param name="frame">
+        /// Tuple containing frame number and value for that frame
+        /// </param>
+        public void AddWaveData(int bodyid, Tuple<int, double> frame)
+        {
+            var trackedBody = ActivePanel.FirstOrDefault(x => x.BodyId == bodyid);
+
+            if (trackedBody == null)
+                return;
+
+            //Body is flagged for recording but has no table assigned
+            if (trackedBody.IsRecording && 
+                HashTblRecord.Contains(trackedBody.BodyId) == false)
+            {
+                HashTblRecord.Add(trackedBody.BodyId, new List<Tuple<int, double>>());
+            }
+
+            if (trackedBody.IsRecording)
+            {
+                //Body is flagged for recording and data will be added
+                var frameList =
+                    (List<Tuple<int, double>>) HashTblRecord[trackedBody.BodyId];
+                frameList.Add(frame);
+            }
+
+            //Insert data into the visible GUI chart
+            UpdateChartFrame(trackedBody.Panel, frame);
+
+        }
+
+        //--------------------------------------------------------------------------------
+        /// <summary>
+        /// Inserts frames of data into the viable chart on the GUI
+        /// </summary>
+        /// <param name="panel">Control panel</param>
+        /// <param name="frame">Tuple containing data for the frame and it's value</param>
+        private void UpdateChartFrame(Panel panel, Tuple<int, double> frame)
+        {
+            //Loop through each control till the chart object comes up
+            foreach (var con in panel.Controls)
+            {
+                if (con.GetType() == typeof (Chart))
+                {
+                    ( (Chart) con ).Series[0].Points.AddXY(frame.Item1%WindowSize, 
+                                                           frame.Item2);
+                    ( (Chart) con ).Refresh();
+                }
+            }
+        }
+        //--------------------------------------------------------------------------------
+        /// <summary>
+        /// Updates the GUI with a change in tracking of a body
+        /// </summary>
+        /// <param name="bodyId">Body of the new tracking event</param>
+        /// <param name="trackingStatus">Status of the body in question</param>
+        public void UpdateTracking(int bodyId, bool trackingStatus)
+        {
+            var targetPanel = ActivePanel.FirstOrDefault(x => x.BodyId == bodyId);
+
+            if (targetPanel == null)
+            {
+                var panel = ActivePanel.FirstOrDefault(x => x.IsActive == false);
+                if (panel != null)
+                {
+                    UpdatePanelDefaults(panel.Panel, trackingStatus);
+                    panel.IsActive    = true;
+                    panel.IsRecording = false;
+                    panel.BodyId      = bodyId;
+                }
+            }
+            else if(trackingStatus == false)
+            {
+                targetPanel.IsActive      = false;
+                targetPanel.IsRecording   = false;
+                targetPanel.BodyId        = -1;
+                targetPanel.Panel.Visible = false;
+
+                if(HashTblRecord.Contains(bodyId))
+                    HashTblRecord.Remove(bodyId);
+
+                RearrangePanels();
+            }
+
+        }
+
+        //--------------------------------------------------------------------------------
+        /// <summary>
+        /// Sets the defaults that the panels are initialized to
+        /// </summary>
+        /// <param name="targetPanel">Panel that is going to be modified</param>
+        /// <param name="showStatus">Status of it's visibility</param>
+        private void UpdatePanelDefaults(Panel targetPanel, bool showStatus)
+        {
+            foreach (Control p in targetPanel.Controls)
+            {
+                if (p.GetType() == typeof (Chart))
+                {
+                    var tmpChart = (Chart) p;
+                    tmpChart.Series[0].Points.Clear();
+                }
+                else if (p.GetType() == typeof (ComboBox))
+                {
+                    SetComboBoxDefualts((ComboBox)p);
+                }
+                else if (p.GetType() == typeof (Button))
+                {
+                    ( (Button) p ).Text = @"Record";
+                    ( (Button) p ).BackColor = DefaultBackColor;
+                }
+            }
+
+            targetPanel.Visible = showStatus;
+        }
+
+
+        //--------------------------------------------------------------------------------
+        /// <summary>
+        /// Set's the default field's of the combo box
+        /// </summary>
+        /// <param name="combo"></param>
+        private void SetComboBoxDefualts(ComboBox combo)
+        {
+            combo.Items.Clear();
+            // ReSharper disable once CoVariantArrayConversion
+            combo.Items.AddRange(GestureList);
+            combo.SelectedItem = combo.Items[0].ToString();
+        }
+
+        //--------------------------------------------------------------------------------
+        /// <summary>
+        /// Obtains the panel in which an object belongs to
+        /// </summary>
+        /// <param name="panel"></param>
+        /// <returns>Tuple pair containing owner panel</returns>
+        private PanelFrame GetPanelFrame(Control panel)
+        {
+            if (panel == null)
+                throw new ArgumentNullException(nameof(panel));
+
+            return ActivePanel.FirstOrDefault(x => x.Panel == panel);
+        }
+
+        //--------------------------------------------------------------------------------
+        /// <summary>
+        /// Event triggered when the record button on the panel is clicked.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void recordBtn_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button) sender;
+            PanelFrame activePanel = GetPanelFrame(clickedButton.Parent);
+
+            if (activePanel == null)
+                return;
+
+            //Get the inverse of the recording type
+            activePanel.IsRecording = !activePanel.IsRecording;
+            if (activePanel.IsRecording)
+            {
+                clickedButton.Text      = @"Stop Recording";
+                clickedButton.BackColor = Color.Red;
+            }
+            else
+            {
+                clickedButton.Text      = @"Record";
+                clickedButton.BackColor = DefaultBackColor;
+            }
+        }
+
+        //--------------------------------------------------------------------------------
+        /// <summary>
+        /// Event triggered when a gesture target is changed. Which will cause an event 
+        /// on tracking watch gesture is processed on the main program.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cb_SelectedGestureChange(object sender, EventArgs e)
+        {
+            ComboBox clickedBox = (ComboBox) sender;
+            var activePanel = GetPanelFrame(clickedBox.Parent);
+
+            //Trigger event that effects main program here
+            var eventArg = new UtilTargetGestArgs(activePanel.BodyId,
+                                                  clickedBox.SelectedItem.ToString());
+
+            OnGestureTargetChange?.Invoke(sender, eventArg);
+        }
+    }
+
+    //====================================================================================
+    /// <summary>
+    /// Container class for keeping track of what panel belongs to what body
+    /// </summary>
+    public class PanelFrame
+    {
+        /// <summary>
+        /// Is the panel active?
+        /// </summary>
+        public bool IsActive { get; set; }
+        /// <summary>
+        /// Is the active panel recording data?
+        /// </summary>
+        public bool IsRecording { get; set; }
+        /// <summary>
+        /// Body index id for the recorded panel
+        /// </summary>
+        public int BodyId { get; set; }
+        /// <summary>
+        /// Panel that contains all necessary visual GUI controls
+        /// </summary>
+        public Panel Panel { get; }
+        public PanelFrame(bool isActive, bool isRecording, int bodyId, Panel panel)
+        {
+            IsActive    = isActive;
+            IsRecording = isRecording;
+            BodyId      = bodyId;
+            Panel       = panel;
+        }
+    }
+}
