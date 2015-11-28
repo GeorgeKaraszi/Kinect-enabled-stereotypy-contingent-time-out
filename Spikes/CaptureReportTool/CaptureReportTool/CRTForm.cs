2c2a46f9a079c7e248aca561d0ffc30c1593b854
George Karaszi <georgekaraszi@gmail.com>
2015-11-28 16:07:01 -0500

    Tool extension spike for the main program. This designed to invoke events that control the actions and results of the GUI for analysis. This tool has been scaled to handle kinects 6 skeletal body tracking too.

---
+++ Spikes/CaptureReportTool/CaptureReportTool/CRTForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaptureReportTool
+{
+    public partial class CRTForm : Form
+    {
+        private UtilWindow _utilWindow;
+        public int TrackedBodies { get; set; }
+
+        public CRTForm()
+        {
+            List<string> GestureNames = new List<string>
+                                        {
+                                            "HandFlapping",
+                                            "HandSideToSide",
+                                            "Rocking"
+                                        };
+            TrackedBodies = 0;
+            _utilWindow = new UtilWindow(GestureNames.ToArray());
+            _utilWindow.OnGestureTargetChange += GestureTargetChange;
+            InitializeComponent();
+        }
+
+        private void GestureTargetChange(object source,
+                                         UtilTargetGestArgs utilTargetGestArgs)
+        {
+            gestureLbl.Text = utilTargetGestArgs.GetBodyId() + @" : " + 
+                              utilTargetGestArgs.GetGestureName();
+        }
+
+        private void openWndBtn_Click(object sender, EventArgs e) { _utilWindow.Show(); }
+
+        private void trackBtn_Click(object sender, EventArgs e)
+        {
+            TrackedBodies = Int32.Parse(activeTracking.Text);
+        }
+
+        private void activeTracking_ValueChanged(object sender, EventArgs e)
+        {
+            NumericUpDown numb = (NumericUpDown) sender;
+            _utilWindow.UpdateTracking(Convert.ToInt32(numb.Value), true);
+        }
+
+        private void rmv_topbtn_Click(object sender, EventArgs e)
+        {
+            _utilWindow.UpdateTracking(1, false);
+        }
+
+        private void rmv_midbtn_Click(object sender, EventArgs e)
+        {
+            _utilWindow.UpdateTracking(2, false);
+        }
+
+        private void rmv_btmbtn_Click(object sender, EventArgs e)
+        {
+            _utilWindow.UpdateTracking(3, false);
+        }
+    }
+}
---
+++ Spikes/CaptureReportTool/CaptureReportTool/Pair.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaptureReportTool
+{
+    //====================================================================================
+    /// <summary>
+    /// Custom tuple class that allows for 3 editable fields
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    public class Pair<T1, T2>
+    {
+        public T1 Item1 { get; set; }
+        public T2 Item2 { get; set; }
+
+        public Pair(T1 item1, T2 item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
+        }
+    }
+
+    //====================================================================================
+    /// <summary>
+    /// Custom tuple class that allows for 3 editable fields
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    public class Pair<T1, T2, T3>
+    {
+        public T1 Item1 { get; set; }
+        public T2 Item2 { get; set; }
+        public T3 Item3 { get; set; }
+        public Pair(T1 item1, T2 item2, T3 item3)
+        {
+            Item1 = item1;
+            Item2 = item2;
+            Item3 = item3;
+        }
+    }
+
+    //====================================================================================
+    /// <summary>
+    /// Custom tuple class that allows for 4 editable fields
+    /// </summary>
+    /// <typeparam name="T1"></typeparam>
+    /// <typeparam name="T2"></typeparam>
+    /// <typeparam name="T3"></typeparam>
+    public class Pair<T1, T2, T3, T4>
+    {
+        public T1 Item1 { get; set; }
+        public T2 Item2 { get; set; }
+        public T3 Item3 { get; set; }
+        public T4 Item4 { get; set; }
+        public Pair(T1 item1, T2 item2, T3 item3, T4 item4)
+        {
+            Item1 = item1;
+            Item2 = item2;
+            Item3 = item3;
+            Item4 = item4;
+        }
+    }
+}
